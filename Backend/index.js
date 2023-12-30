55982a77877e29f52cb4e005281887b5e0ed81c6
AdityaAkangire <adityaakangire1@gmail.com>
2023-12-30 17:53:02 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+
+app.get("/", (req, res) => {
+  console.log(path.join(__dirname, "../public"));
+  const foodData = [
+    {
+      name: "Boiled Egg",
+      price: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/egg.png",
+      type: "breakfast",
+    },
+    {
+      name: "RAMEN",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/ramen.png",
+      type: "lunch",
+    },
+    {
+      name: "GRILLED CHICKEN",
+      price: 45,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/chicken.png",
+      type: "dinner",
+    },
+    {
+      name: "CAKE",
+      price: 18,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/cake.png",
+      type: "breakfast",
+    },
+    {
+      name: "BURGER",
+      price: 23,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/burger.png",
+      type: "lunch",
+    },
+    {
+      name: "PANCAKE",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/pancake.png",
+      type: "dinner",
+    },
+  ];
+
+  res.json(foodData);
+});
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
